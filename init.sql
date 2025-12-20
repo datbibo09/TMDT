c9fdb5dd76a8143b001da464890d34820f16e9ef
Phạm Đình Đạt <phamdinhdat4@gmail.com>
2025-12-20 21:03:06 +0700

    0

--- init.sql
+++ init.sql
@@ -3,6 +3,7 @@ CREATE DATABASE IF NOT EXISTS product_db;
 CREATE DATABASE IF NOT EXISTS inventory_db;
 CREATE DATABASE IF NOT EXISTS auth_db;
 CREATE DATABASE IF NOT EXISTS payment_db;
+CREATE DATABASE IF NOT EXISTS orders_db;
 -- Cấp quyền (Optional - Docker MySQL image thường đã xử lý root, nhưng thêm cho chắc chắn với user thường)
 GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
 FLUSH PRIVILEGES;
